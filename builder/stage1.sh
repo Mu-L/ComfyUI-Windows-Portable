0f9847d74c7fe64a4294d789c60b06fa77478162
YAN Wenkun <code@yanwk.fun>
2026-04-17 15:45:30 +0800

    Update tools

--- builder/stage1.sh
+++ builder/stage1.sh
@@ -12,7 +12,7 @@ ls -lahF
 
 # Download Python Standalone
 curl -sSL \
-https://github.com/astral-sh/python-build-standalone/releases/download/20260320/cpython-3.13.12+20260320-x86_64-pc-windows-msvc-install_only.tar.gz \
+https://github.com/astral-sh/python-build-standalone/releases/download/20260414/cpython-3.13.13+20260414-x86_64-pc-windows-msvc-install_only.tar.gz \
     -o python.tar.gz
 tar -zxf python.tar.gz
 mv python python_standalone
